da19bea1265394c949196257f0819c5e81189afd
Erik Visser <92474318+Erikvisser22@users.noreply.github.com>
2022-12-02 12:17:41 +0100

    Update utils.py

--- easy_tenants/utils.py
+++ easy_tenants/utils.py
@@ -10,6 +10,7 @@ def get_state():
     state_default = {
         "enabled": True,
         "tenant": None,
+        "is_admin": False
     }
     state = getattr(state_local, "state", state_default)
     return state
@@ -23,14 +24,19 @@ def get_current_tenant():
 
     return state["tenant"]
 
+def is_admin():
+    state = get_state()
+    return state["is_admin"]
+
 
 @contextmanager
-def tenant_context(tenant=None, enabled=True):
+def tenant_context(tenant=None, enabled=True, is_admin=False):
     previous_state = get_state()
 
     new_state = previous_state.copy()
     new_state["enabled"] = enabled
     new_state["tenant"] = tenant
+    new_state["is_admin"] = is_admin
 
     state_local.state = new_state
 
@@ -41,6 +47,6 @@ def tenant_context(tenant=None, enabled=True):
 
 
 @contextmanager
-def tenant_context_disabled():
-    with tenant_context(enabled=False):
+def tenant_context_disabled(is_admin = False):
+    with tenant_context(enabled=False, is_admin=is_admin):
         yield
